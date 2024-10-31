e2f47600b4f179b6733d81b18fdf2a4fb213ae5c
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-10-31 11:34:19 +0300

    add tests to CI

--- src/test/java/ru/tyatyushkin/telegram/MainTest.java
+++ src/test/java/ru/tyatyushkin/telegram/MainTest.java
@@ -1,4 +1,15 @@
 package ru.tyatyushkin.telegram;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class MainTest {
+
+    @Test
+    public void testMain() {
+        // This is a simple test to check if the main method runs without exceptions
+        Main.main(new String[]{});
+        assertEquals(1, 1, "This is a placeholder test");
+    }
+
 }
