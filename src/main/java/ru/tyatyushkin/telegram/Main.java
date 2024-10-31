41eb0b4065a05bab6a2322e1d24f17e25d2f1e38
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-10-31 22:18:07 +0300

    add method getUpdates

--- src/main/java/ru/tyatyushkin/telegram/Main.java
+++ src/main/java/ru/tyatyushkin/telegram/Main.java
@@ -1,8 +1,28 @@
 package ru.tyatyushkin.telegram;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Main {
-    public static void main(String[] args) {
-        String message = "Hello!";
-        System.out.println(message);
+    public static void main(String[] args) throws Exception {
+        String app_token = System.getenv("TG_TOKEN");
+
+        Bot ma = new Bot(app_token);
+
+        String updates = ma.getUpdates();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(updates);
+        System.out.println(jsonNode.toPrettyString());
+
+        while (true) {
+            try {
+                Thread.sleep(2000);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }
     }
 }
