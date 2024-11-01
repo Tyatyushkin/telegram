85f5c8388c7bc9a8f463724aa21919f8ea8fbd49
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-11-01 18:36:38 +0300

    generate code with copilot

--- src/main/java/ru/tyatyushkin/telegram/X.java
+++ src/main/java/ru/tyatyushkin/telegram/X.java
@@ -6,4 +6,41 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class X {
+    static final String API_URL = "https://api.x.com/2/tweets";
+    private final String bearerToken;
+
+    public X(String bearerToken) {
+        this.bearerToken = bearerToken;
+    }
+
+    public String getTweets(String query) {
+        StringBuilder content = new StringBuilder();
+
+        try {
+            URL url = new URL(API_URL + "?query=" + query);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Authorization", "Bearer " + bearerToken);
+
+            int responseCode = conn.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+
+                in.close();
+            } else {
+                System.err.println("HTTP error code: " + responseCode);
+            }
+
+            conn.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return content.toString();
+    }
 }
