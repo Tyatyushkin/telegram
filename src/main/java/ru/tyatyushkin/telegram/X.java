f7151194497b9943a521a06e4c1ca6a4b76265fd
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-11-01 18:32:49 +0300

    create class X

---
+++ src/main/java/ru/tyatyushkin/telegram/X.java
@@ -0,0 +1,9 @@
+package ru.tyatyushkin.telegram;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class X {
+}
