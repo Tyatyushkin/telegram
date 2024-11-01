1a8e7fb479f4ce61f15c7675080060f8d451ff99
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-11-01 04:36:08 +0300

    modify getUpdates

--- src/main/java/ru/tyatyushkin/telegram/Bot.java
+++ src/main/java/ru/tyatyushkin/telegram/Bot.java
@@ -14,20 +14,26 @@ public class Bot {
         this.token = token;
     }
 
-    public String getUpdates() throws Exception {
-        URL url = new URL(API_URL + token + "/getUpdates");
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setDoOutput(true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String inputLine;
+    public String getUpdates() {
         StringBuilder content = new StringBuilder();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
-        }
 
-        in.close();
-        conn.disconnect();
+        try {
+            URL url = new URL(API_URL + token + "/getUpdates");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setDoOutput(true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+
+            in.close();
+            conn.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return content.toString();
     }
