3330b33e2c561ed4210c838732417efe6f27eedc
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-11-05 21:06:02 +0300

    create Telegram.java class

---
+++ src/main/java/ru/tyatyushkin/telegram/Telegram.java
@@ -0,0 +1,11 @@
+package ru.tyatyushkin.telegram;
+
+public class Telegram {
+
+    //TODO перенести методы и переменные из Bot.java в этот класс
+
+    public void sendMessage(String chatId, String message) {
+
+
+    }
+}
