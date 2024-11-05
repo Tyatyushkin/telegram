9b4942ad37ce90bf6523305089c1c226fb124e0c
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-11-05 07:38:21 +0300

    change message description

--- src/main/java/ru/tyatyushkin/telegram/Main.java
+++ src/main/java/ru/tyatyushkin/telegram/Main.java
@@ -30,7 +30,7 @@ public class Main {
                 newTweet = twitter.getLastTweetByUserId(userID);
                 xCount = 0;
                 if (!newTweet.equals(lastTweet)) {
-                    ma.sendMessage(chatID, "Что же там нового у нашего Максимки?:  " + twitter.getMessageByMessageId(newTweet).replaceAll("\\n", " "));
+                    ma.sendMessage(chatID, "Какие есть новости от Мишеньки?:  " + twitter.getMessageByMessageId(newTweet).replaceAll("\\n", " "));
                     lastTweet = newTweet;
                 }
             } else {
