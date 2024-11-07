43826868954ec1784fc43b8cf5278fc462236388
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-11-07 13:09:13 +0300

    modify Main.java

--- src/main/java/ru/tyatyushkin/telegram/Main.java
+++ src/main/java/ru/tyatyushkin/telegram/Main.java
@@ -22,59 +22,8 @@ public class Main {
             Bot test = new Bot(app_token);
             test.createTestBot();
         } else {
-            Bot ma = new Bot(app_token);
-            X twitter = new X(x_token);
-            userID = twitter.getUserIdByUsername(x_username);
-            int xCount = 240;
-            String lastTweet = null;
-            String  newTweet;
-
-            while (true) {
-                //add X methods
-                if (xCount >= xTimer ) {
-                    newTweet = twitter.getLastTweetByUserId(userID);
-                    xCount = 0;
-                    if (newTweet != null && !newTweet.equals(lastTweet) ) {
-                        ma.sendMessage(chatID, "Post from X:  " + twitter.getMessageByMessageId(newTweet).replaceAll("\\n", " "));
-                        lastTweet = newTweet;
-                    }
-                } else {
-                    xCount++;
-                }
-
-                String updates = ma.getUpdates();
-                if (updates != null) {
-                    ObjectMapper objectMapper = new ObjectMapper();
-                    JsonNode jsonNode = objectMapper.readTree(updates);
-                    JsonNode resultArray = jsonNode.get("result");
-
-                    for (JsonNode update : resultArray) {
-                        int updateId = update.get("update_id").asInt();
-                            JsonNode messageNode = update.get("message");
-                        if (messageNode != null && messageNode.get("text") != null) {
-                            String text = messageNode.get("text").asText();
-                            String chatId = messageNode.get("chat").get("id").asText();
-
-                            if (text.toLowerCase().startsWith("gpt")) {
-                                ma.sendMessage(chatId, "Адвокат еще написал методы взаимодействия с chatGPT, но он очень старается!");
-                            }
-                            if (text.toLowerCase().contains("сиськи")) {
-                                ma.sendMessage(chatId, "Мастерплан еще не придумал как сюда загружать фото сисек, но он о них постоянно думает!");
-                            }
-                            if (text.toLowerCase().contains("python")) {
-                                ma.sendMessage(chatId, "Кому что? А ебуняке лишь бы питона душить");
-                            }
-                        }
-                        Bot.setLastUpdateId(updateId);
-                    }
-                }
-                try {
-                    Thread.sleep(5000);
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+           Bot telegram = new Bot(app_token);
+           telegram.createBot();
         }
     }
 }
