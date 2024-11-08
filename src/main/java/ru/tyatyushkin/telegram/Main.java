e40a3e6641d9f696473869b4bd8c442414ea7016
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-11-08 08:08:11 +0300

    refactor main method

--- src/main/java/ru/tyatyushkin/telegram/Main.java
+++ src/main/java/ru/tyatyushkin/telegram/Main.java
@@ -1,17 +1,10 @@
 package ru.tyatyushkin.telegram;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         boolean test_mode = Boolean.parseBoolean(System.getenv("TEST_MODE"));
         String app_token = System.getenv("TG_TOKEN");
-        String x_token = System.getenv("X_TOKEN");
-        String chatID = System.getenv("CHAT_ID");
-        String x_username = System.getenv("X_USERNAME");
-        int xTimer = 240;
-        String userID;
 
         if (app_token == null) {
             System.out.println("Переменная окружения TG_TOKEN не задана!");
