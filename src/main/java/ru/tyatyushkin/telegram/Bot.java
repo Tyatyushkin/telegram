43964584c2971edc11d588dc4da131b3544c5a59
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-11-08 10:35:28 +0300

    remove unused import

--- src/main/java/ru/tyatyushkin/telegram/Bot.java
+++ src/main/java/ru/tyatyushkin/telegram/Bot.java
@@ -2,12 +2,6 @@ package ru.tyatyushkin.telegram;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.HttpURLConnection;
-
 import java.util.concurrent.TimeUnit;
 
 
