88027523579d4b4abc5356da72e14afcac148b07
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-11-09 07:45:33 +0300

    modify import

--- src/main/java/ru/tyatyushkin/telegram/Bot.java
+++ src/main/java/ru/tyatyushkin/telegram/Bot.java
@@ -1,6 +1,5 @@
 package ru.tyatyushkin.telegram;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONArray;
