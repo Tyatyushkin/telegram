52655e4c2e4368413259b6c41b212cf1b916f785
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-11-14 19:30:41 +0300

    create class for logging and use in Main.java

--- src/main/java/ru/tyatyushkin/telegram/Main.java
+++ src/main/java/ru/tyatyushkin/telegram/Main.java
@@ -1,18 +1,16 @@
 package ru.tyatyushkin.telegram;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 
 public class Main {
-    private static final Logger logger = LogManager.getLogger(Main.class);
-
     public static void main(String[] args)  {
+        LoggerConfig.initialize();
+
         boolean test_mode = Boolean.parseBoolean(System.getenv("TEST_MODE"));
         String app_token = System.getenv("TG_TOKEN");
 
         if (app_token == null) {
-            logger.error("Ошибка: Переменная окружения TG_TOKEN не задана!");
+            LoggerConfig.logger.error("Ошибка: Переменная окружения TG_TOKEN не задана!");
             System.exit(1);
         }
 
