3bd78e53814dc6836d5d58bb8273ce95f9c9efd8
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-11-14 21:51:32 +0300

    create template for method lastVideo

--- src/main/java/ru/tyatyushkin/telegram/Youtube.java
+++ src/main/java/ru/tyatyushkin/telegram/Youtube.java
@@ -1,4 +1,27 @@
 package ru.tyatyushkin.telegram;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Youtube {
+    private static final String API_KEY = "YOUR_API_KEY";
+    private static final String CHANNEL_ID = "YOUR_CHANNEL_ID";
+
+
+    public String lastVideo(String channelId) {
+        try {
+            String urlString = String.format(
+                    "https://www.googleapis.com/youtube/v3/search?key=%s&channelId=%s&part=snippet&order=date&maxResults=5",
+                    API_KEY, CHANNEL_ID);
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            conn.disconnect();
+        } catch (Exception e) {
+            LoggerConfig.logger.error("An error occurred while fetching the last video: ", e);
+        }
+        return null;
+    }
 }
