2fa37cc145a241ea462c53d39bd5f0d850e38f9b
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-11-23 11:20:33 +0300

    add channelId var

--- src/main/java/ru/tyatyushkin/telegram/Youtube.java
+++ src/main/java/ru/tyatyushkin/telegram/Youtube.java
@@ -4,18 +4,17 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class Youtube {
-    private final String token;
-    private static final String CHANNEL_ID = "YOUR_CHANNEL_ID";
+    private final String token;;
 
     public Youtube(String token){
         this.token = token;
     }
 
-    public String lastVideo(String channelId) {
+    public void lastVideo(String channelId) {
         try {
             String urlString = String.format(
                     "https://www.googleapis.com/youtube/v3/search?key=%s&channelId=%s&part=snippet&order=date&maxResults=5",
-                    token, CHANNEL_ID);
+                    token, channelId);
             URL url = new URL(urlString);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
@@ -25,6 +24,5 @@ public class Youtube {
         } catch (Exception e) {
             LoggerConfig.logger.error("An error occurred while fetching the last video: ", e);
         }
-        return null;
     }
 }
