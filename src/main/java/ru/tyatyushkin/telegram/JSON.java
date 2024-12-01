bc66f51595b78a84ebbc3810516b2828ab7d0d66
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-12-01 10:37:37 +0300

    create class JSON.java for work with json

---
+++ src/main/java/ru/tyatyushkin/telegram/JSON.java
@@ -0,0 +1,22 @@
+package ru.tyatyushkin.telegram;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JSON {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private boolean isCheck(String messages) {
+        return messages != null;
+    }
+
+    private String getResult(String messages) throws JsonProcessingException {
+        if (isCheck(messages)) {
+            JsonNode rootNode = objectMapper.readTree(messages);
+            JsonNode resultArray = rootNode.get("result");
+            return resultArray.toString();
+        }
+        return null;
+    }
+}
