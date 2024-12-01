5f2483a9c850f9245ae0324131579f037b900ea7
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-12-01 10:25:50 +0300

    remove unused libs

--- src/main/java/ru/tyatyushkin/telegram/Bot.java
+++ src/main/java/ru/tyatyushkin/telegram/Bot.java
@@ -4,11 +4,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONArray;
 import org.json.JSONObject;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
 
