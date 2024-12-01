dbc3b11d08d614dd7036ba999b1af912fd0e6d0c
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-12-02 01:50:30 +0300

    remove unused libs

--- src/main/java/ru/tyatyushkin/telegram/JSON.java
+++ src/main/java/ru/tyatyushkin/telegram/JSON.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
-import java.util.ArrayList;
 
 
 public class JSON {
